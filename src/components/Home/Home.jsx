d35f9f4278ae55b7bae82b052dd570ddb17fee9d
partha2001ps <parthapn2017@gmail.com>
2023-12-08 20:07:17 +0530

    home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import './home.css'
+import React, { useEffect, useState } from 'react';
+import './home.css';
+
 function Home() {
+  const [text, setText] = useState("FULL STACK DEVELOPER (MERN)");
+
+  const animateTitle = (text, setText) => {
+    setText('');
+    text.split('').forEach((letter, index) => {
+      setTimeout(() => {
+        setText(prevText => prevText + letter);
+      }, index * 300);
+    });
+  };
+
+  useEffect(() => {
+    animateTitle('FULL STACK DEVELOPER (MERN)', setText);
+
+    const intervalId = setInterval(() => {
+      animateTitle('FULL STACK DEVELOPER (MERN)', setText);
+    }, 9000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <section id='Home' className='pro'>
-    <div >
-      <h1 className='greeting'>Hello, it's Me <span className='name'>Parthasarathi </span></h1>
-        <p className='developer-description'>And I'm Full Stack Developer (MERN)</p>
+      <div className='left'>
+        <h1 className='greeting'>Hello, it's Me <span className='name'>Parthasarathi </span></h1>
+        <p className='developer-description'>And I'm <span className='fsd'>{text}</span></p>
         <p className='additional-content'>
-      I specialize in building end-to-end web applications using the MERN stack. With a strong foundation in both frontend and backend technologies, I bring creativity and efficiency to every project.
-    </p></div>
-      <img className='profile-image' src="/image/photo.png" alt="" />  
-  </section>
-  
-  )
+          I specialize in building end-to-end web applications using the MERN stack. With a strong foundation in both frontend and backend technologies, I bring creativity and efficiency to every project.
+        </p>
+      </div>
+      <img className='profile-image' src="/image/photo.png" alt="" />
+    </section>
+  );
 }
 
-export default Home
+export default Home;
