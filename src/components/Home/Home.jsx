f73f8272cc9a90d0504be33b1b2687882c54d7b0
partha2001ps <parthapn2017@gmail.com>
2023-12-09 17:31:53 +0530

    Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './home.css';
 
 function Home() {
   const [text, setText] = useState("FULL STACK DEVELOPER (MERN)");
+  const contactRef = useRef(null);
 
   const animateTitle = (text, setText) => {
     setText('');
@@ -23,6 +24,16 @@ function Home() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const handleResumeClick = () => {
+    const resumeUrl = 'https://drive.google.com/file/d/11FR_gQYw_ddH-i5u0_htY_9KDRIsADNV/view';
+    window.open(resumeUrl, '_blank'); 
+  };
+  const scrollToContact = () => {
+    if (contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <section id='Home' className='pro'>
       <div className='left'>
@@ -31,8 +42,12 @@ function Home() {
         <p className='additional-content'>
           I specialize in building end-to-end web applications using the MERN stack. With a strong foundation in both frontend and backend technologies, I bring creativity and efficiency to every project.
         </p>
+        <section id='Content'>
+          <button onClick={scrollToContact}>Contact</button><button onClick={handleResumeClick}>View Resume</button>
+        </section>
       </div>
-      <img className='profile-image' src="/image/photo.png" alt="" />
+   <img className='profile-image' src="/image/photo.png" alt="profile-image" />
+      <div ref={contactRef}></div>
     </section>
   );
 }
